5f0a1c7aa15cc206266dbb1ea97bf5cbe2beddf9
Chiara Yen <chiyen.chiara@gmail.com>
2020-01-01 21:18:00 +0800

    day 4 question

---
+++ day4/input.py
@@ -0,0 +1,12 @@
+
+'''
+Password criteria:
+
+1. It is a six-digit number.
+2. The value is within the range given in your puzzle input.
+3. Two adjacent digits are the same (like 22 in 122345).
+4. Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+
+'''
+
+password_range = '402328-864247'
---
+++ day4/test.py
@@ -0,0 +1,10 @@
+password_range = '402328-864247'
+
+# meets criteria
+a = '111111'
+
+# does not meet criteria
+b = '223450'
+
+# does not meet criteria
+c = '123789'
