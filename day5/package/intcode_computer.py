6c03f7b35e0857c948ec33d20f6771773ddc1c66
Chiara Yen <chiyen.chiara@gmail.com>
2020-01-18 17:26:17 +0800

    day 5 - 1: extract some constants in intcode_computer

--- day5/package/intcode_computer.py
+++ day5/package/intcode_computer.py
@@ -1,5 +1,16 @@
 import math
 
+# modes
+POSITION_MODE = 0
+IMMEDIATE_MODE = 1
+
+# opcodes
+OPCODE_HALT = 99
+OPCODE_ADD = 1
+OPCODE_MULTIPLY = 2
+OPCODE_INPUT = 3
+OPCODE_OUTPUT = 4
+
 
 def add(x, y):
     return x + y
@@ -10,25 +21,30 @@ def multiply(x, y):
 
 
 def handle_opcode(opcode, params, list):
-    if opcode is 99:
+    if opcode is OPCODE_HALT:
         return list
-    elif opcode is 1:
+
+    elif opcode is OPCODE_ADD:
         index0 = params[0]
         index1 = params[1]
         position = params[2]
         list[position] = add(list[index0], list[index1])
-    elif opcode is 2:
+
+    elif opcode is OPCODE_MULTIPLY:
         index0 = params[0]
         index1 = params[1]
         position = params[2]
         list[position] = multiply(list[index0], list[index1])
-    elif opcode is 3:
+
+    elif opcode is OPCODE_INPUT:
         value = params[0]
         position = params[0]
         list[position] = value
-    elif opcode is 4:
+
+    elif opcode is OPCODE_OUTPUT:
         position = params[0]
         return list[position]
+
     else:
         raise TypeError('unknow opcode =', opcode)
     return list
@@ -39,17 +55,17 @@ def get_final_list(list):
     pointer = 0
     while pointer < len(list):
         opcode = list[pointer]
-        if opcode is 99:
+        if opcode is OPCODE_HALT:
             return list
-        elif opcode is 1 or opcode is 2:
+        elif opcode is OPCODE_ADD or opcode is OPCODE_MULTIPLY:
             params = 3
-        elif opcode is 3 or opcode is 4:
+        elif opcode is OPCODE_INPUT or opcode is OPCODE_OUTPUT:
             params = 1
 
         end = (pointer + 1) + params
         params = list[pointer + 1:end]
         result = handle_opcode(opcode, params, list)
-        if opcode is 4:
+        if opcode is OPCODE_OUTPUT:
             return result
         pointer += end
     return list
